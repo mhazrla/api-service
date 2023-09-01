55ccbe5a58ff04aff1d05ea2293f7506f66615ee
mhazrla <mhazrla@gmail.com>
2023-09-01 10:46:40 +0700

    first commit

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
+import Tour from "./db/models/Tour";
+import Travel from "./db/models/Travel";
 
 export const createServer = () => {
   const app = express();
@@ -15,10 +17,24 @@ export const createServer = () => {
     return res.json({ ok: true, environment: process.env.NODE_ENV });
   });
 
-  
-
   app.get("/message/:name", (req, res) => {
     return res.json({ message: `hello ${req.params.name}` });
+  });
+
+  app.post("/api/travels", async (req, res) => {
+    const travel = await Travel.create(req.body);
+    return res.status(201).json(travel);
+  });
+
+  app.post("/api/travels/:id/tours", async (req, res) => {
+    const data = { travel_id: req.params.id, ...req.body };
+    const tour = await Tour.create(data);
+    return res.status(201).json(tour);
+  });
+
+  app.get("/api/travels", async (req, res) => {
+    const travels = await Tour.findAll({ include: [Tour] });
+    return res.status(200).json(travels);
   });
 
   return app;
